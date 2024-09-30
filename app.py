1b7cb587768d956eb47f318ea6359d41eeb8d58d
Joe <josepilove@gmail.com>
2024-09-30 12:53:32 -0500

    next version upload ZZ

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+from config.config import SECRET_KEY
+from routes.main import main
+from routes.report import report
 
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# Register blueprints
+app.register_blueprint(main)
+app.register_blueprint(report)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
